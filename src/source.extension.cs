60e01418faf464e72d48184023faea371e814032
Alistair Chapman <alistairgchapman@gmail.com>
2016-08-03 22:28:29 +1000

    Updates to README and update extension name

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -8,11 +8,11 @@ namespace Cake.VisualStudio
     static class Vsix
     {
         public const string Id = "3cf9b016-d63f-44ee-849d-6f3efc996134";
-        public const string Name = "Cake Task Runner";
-        public const string Description = "Adds support for Cake build scripts in Visual Studio 2015's Task Runner Explorer.";
+        public const string Name = "Cake for Visual Studio";
+        public const string Description = "Adds support for the Cake build tool in Visual Studio 2015. Includes support for the Task Runner Explorer, new templates and bootstrapping important Cake files.";
         public const string Language = "en-US";
         public const string Version = "0.1.1";
-        public const string Author = "Alistair Chapman";
+        public const string Author = "Cake";
         public const string Tags = "cake, build";
     }
 }
