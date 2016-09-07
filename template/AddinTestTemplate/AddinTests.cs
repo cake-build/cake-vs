04ba8dda6aacc36d683d2dde2a4ae29037284e82
Alistair Chapman <alistairgchapman@gmail.com>
2016-09-07 12:05:53 +1000

    Add new addin test template

---
+++ template/AddinTestTemplate/AddinTests.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Xunit;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+	public class AddinTests
+    {
+        [Fact]
+        public void ShouldThrowIfFileNotExists()
+        {
+            // arrange
+            var fixture = new CakeFixture(false);
+            //act
+            var result = Record.Exception(() => fixture.Run());
+            //assert
+            Assert.IsType(typeof(FileNotFoundException), result);
+        }
+
+        [Theory]
+        [InlineData("./build.cake")]
+        [InlineData("./deploy.cake")]
+        public void ShouldIncludeFileName(string scriptFilePath)
+        {
+            //arrange
+            var fixture = new CakeFixture(true, scriptFilePath);
+            //act
+            var result = fixture.Run();
+            //result
+            Assert.True(result.Args.Contains(scriptFilePath));
+        }
+    }
+}
