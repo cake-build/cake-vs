ca53b738b26c26bf3c3453dfbc2198486c8dd1ed
Alistair Chapman <alistairgchapman@gmail.com>
2016-10-18 19:42:58 +1000

    Added support for "smart" indentation in Cake files. Indents are added on method body open, otherwise last offset used.

---
+++ src/Editor/SmartIndentProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Cake.VisualStudio.Helpers;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Cake.VisualStudio.Editor
+{
+    [Export(typeof(ISmartIndentProvider))]
+    [ContentType(Constants.CakeContentType)]
+    class SmartIndentProvider : ISmartIndentProvider
+    {
+        [Import] private IEditorOptionsFactoryService Factory { get; set; }
+
+        public ISmartIndent CreateSmartIndent(ITextView textView)
+        {
+            if (textView == null)
+            {
+                throw new ArgumentNullException(nameof(textView));
+            }
+
+            return Factory == null
+                ? new SmartIndent(textView)
+                : new SmartIndent(textView, Factory.GetOptions(textView));
+        }
+    }
+}
