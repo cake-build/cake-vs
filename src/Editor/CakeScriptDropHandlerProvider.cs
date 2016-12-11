968450d011ed4004241af0ed63a48dbd47057123
Alistair Chapman <alistair@cakebuild.net>
2016-12-12 01:04:18 +1000

    Add support for drag-and-drop with cake and dll files

---
+++ src/Editor/CakeScriptDropHandlerProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.VisualStudio.Classifier.Languages;
+using Cake.VisualStudio.Helpers;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.DragDrop;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Cake.VisualStudio.Editor
+{
+    [Export(typeof(IDropHandlerProvider))]
+    [DropFormat("CF_VSSTGPROJECTITEMS")]
+    [DropFormat("CF_VSREFPROJECTITEMS")]
+    [DropFormat("FileDrop")]
+    [Name("CakeDropHandler")]
+    [ContentType(Constants.CakeContentType)]
+    [Order(Before = "DefaultFileDropHandler")]
+    class CakeScriptDropHandlerProvider : IDropHandlerProvider
+    {
+        [Import]
+        ITextDocumentFactoryService TextDocumentFactoryService { get; set; }
+
+        public IDropHandler GetAssociatedDropHandler(IWpfTextView wpfTextView)
+        {
+            ITextDocument document;
+
+            if (TextDocumentFactoryService.TryGetTextDocument(wpfTextView.TextBuffer, out document))
+            {
+                return
+                    wpfTextView.Properties.GetOrCreateSingletonProperty(
+                        () => new CakeScriptDropHandler(wpfTextView, document));
+            }
+
+            return null;
+        }
+    }
+}
