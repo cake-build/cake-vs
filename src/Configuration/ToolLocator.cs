29f243edb5ccef2cff8cd3d4bc52b3dad360e7d7
Gary Ewan Park <gep13@gep13.co.uk>
2020-12-18 21:41:29 +0000

    (maint) Formatting changes

--- src/Configuration/ToolLocator.cs
+++ src/Configuration/ToolLocator.cs
@@ -2,21 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Cake.VisualStudio.Helpers;
-using Microsoft.VisualStudio.Shell;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Cake.VisualStudio.Helpers;
+using Microsoft.VisualStudio.Shell;
 
 namespace Cake.VisualStudio.Configuration
 {
     internal sealed class ToolLocator
     {
-        private readonly string _executable;
-        private List<string> KnownPaths { get; set; } = new List<string>();
+        private readonly List<string> _executableNames;
+        private List<string> _knownPaths { get; set; } = new List<string>();
 
         public ToolLocator(string executableName)
         {
@@ -26,33 +24,37 @@ namespace Cake.VisualStudio.Configuration
         public ToolLocator AddEnvironmentVariables(string variable = "CAKE_PATHS_TOOLS")
         {
             var env = Environment.GetEnvironmentVariable(variable);
+
             if (!string.IsNullOrWhiteSpace(env) && Directory.Exists(env))
             {
-                KnownPaths.Add(env);
+                _knownPaths.Add(env);
             }
+
             return this;
         }
 
         public ToolLocator AddKnownPaths(params string[] paths)
         {
-            KnownPaths.AddRange(paths);
+            _knownPaths.AddRange(paths);
             return this;
         }
 
         public ToolLocator AddConfigPath(Func<ConfigurationParser> configFunc)
         {
             var path = configFunc?.Invoke()?.GetToolsPath();
+
             if (path != null)
             {
-                KnownPaths.Add(path);
-                KnownPaths.Add(Path.Combine(path, "Cake"));
+                _knownPaths.Add(path);
+                _knownPaths.Add(Path.Combine(path, "Cake"));
             }
+
             return this;
         }
 
         public string Locate(string workingDirectory)
         {
-            foreach (var path in KnownPaths.Select(p => p.TrimPrefix("./")))
+            foreach (var path in _knownPaths.Select(p => p.TrimPrefix("./")))
             {
                 var fullPath = Path.Combine(workingDirectory, path, _executable);
                 if (File.Exists(fullPath)) return fullPath;
